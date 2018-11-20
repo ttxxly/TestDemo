25b6f8d180e154a1e98cf4102910d5f15e87ef8b
ttxxly <ttxxly@gmail.com>
2018-11-20 16:25:39 +0800

    完成 OrderVo。

--- src/main/java/com/skm/demo/web/vo/OrderVo.java
+++ src/main/java/com/skm/demo/web/vo/OrderVo.java
@@ -1,4 +1,100 @@
 package com.skm.demo.web.vo;
 
+import java.util.Date;
+
 public class OrderVo {
+
+    /**
+     * 主键id
+     */
+    private Long id;
+
+    /**
+     * 采购单号
+     */
+    private String no;
+
+    /**
+     * 货主编码
+     */
+    private String shipper_code;
+
+    /**
+     * 供货商编码
+     */
+    private String provider_code;
+
+    /**
+     * 供货商名称
+     */
+    private String provider_name;
+
+    /**
+     * 订单提交时间
+     */
+    private Date submit_dt;
+
+    /**
+     * 到货日期
+     */
+    private Date delivery_dt;
+
+    public OrderVo() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getShipper_code() {
+        return shipper_code;
+    }
+
+    public void setShipper_code(String shipper_code) {
+        this.shipper_code = shipper_code;
+    }
+
+    public String getProvider_code() {
+        return provider_code;
+    }
+
+    public void setProvider_code(String provider_code) {
+        this.provider_code = provider_code;
+    }
+
+    public String getProvider_name() {
+        return provider_name;
+    }
+
+    public void setProvider_name(String provider_name) {
+        this.provider_name = provider_name;
+    }
+
+    public Date getSubmit_dt() {
+        return submit_dt;
+    }
+
+    public void setSubmit_dt(Date submit_dt) {
+        this.submit_dt = submit_dt;
+    }
+
+    public Date getDelivery_dt() {
+        return delivery_dt;
+    }
+
+    public void setDelivery_dt(Date delivery_dt) {
+        this.delivery_dt = delivery_dt;
+    }
 }
